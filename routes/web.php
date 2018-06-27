e7711a8dca29a445b5a89255dabb6a42de0c72de
RyanZhou <goodfriend3636@gmail.com>
2018-06-27 16:34:11 +0800

    add frontend views

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('frontend.index');
+})->name('home');
+
+Route::get('about', function () {
+    return view('frontend.about');
+})->name('about');
+
+Route::get('products', function () {
+    return view('frontend.products');
+})->name('products');
+Route::get('store', function () {
+    return view('frontend.store');
+})->name('store');
